6adb0750593e445768c8ef472bb86f10d0c6163a
Abdelrahman Elkady <elkadyabdelrahman@gmail.com>
2019-10-14 10:17:20 +0200

    update docker compose aliases

--- aliases/docker.sh
+++ aliases/docker.sh
@@ -1,4 +1,8 @@
-alias dco='docker-compose'
+alias compose-up='docker-compose up'
+alias compose-up-build='docker-compose up --build'
+
+alias compose-down='docker-compose down'
+alias compose-down-f='docker-compose down --rmi local -v'
 
 # Run docker in interactive mode using (bash)
 # defaults to the current directory name as the container name if no argument is passed
