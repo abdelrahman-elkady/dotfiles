b58f2745da6f52f7e00533c3c4e19832359e0828
Abdelrahman Elkady <elkadyabdelrahman@gmail.com>
2019-11-06 14:00:29 +0200

    update compose aliases

--- aliases/docker.sh
+++ aliases/docker.sh
@@ -1,12 +1,12 @@
 alias compose-up='docker-compose up'
-alias cup="docker-compose up -d --build && d-interactive"
+alias cup="docker-compose up -d --build && dinteractive"
 
-alias compose-down='docker-compose down'
-alias cdown='docker-compose down --rmi local -v'
+alias cdown='docker-compose down'
+alias cdown-f='docker-compose down --rmi local -v'
 
 # Run docker in interactive mode using (bash)
 # defaults to the current directory name as the container name if no argument is passed
-function d-interactive() {
+function dinteractive() {
     if [[ -z "$1" ]]; then
         shift # discard the argument (the container name)
         command docker exec -it ${PWD##*/} bash
