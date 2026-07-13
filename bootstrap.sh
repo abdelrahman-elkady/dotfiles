d146b371768b7ec17f6729d1ae33e8203ad3b234
Abdelrahman Elkady <elkadyabdelrahman@gmail.com>
2026-07-14 01:21:13 +0300

    chore: cleanup unused config

--- bootstrap.sh
+++
@@ -1,2 +0,0 @@
-# install emojify
-sudo sh -c "curl https://raw.githubusercontent.com/mrowa44/emojify/master/emojify -o /usr/local/bin/emojify && chmod +x /usr/local/bin/emojify"
--- install.sh
+++ install.sh
@@ -2,9 +2,8 @@
 
 # Get current dir (so run this script from anywhere)
 
-export DOTFILES_DIR EXTRA_DIR
+export DOTFILES_DIR
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-EXTRA_DIR="$HOME/.extra"
 
 mkdir -p "$HOME/.config"
 
